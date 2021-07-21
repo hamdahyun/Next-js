0f863b03b659f794e43e3def51d02c855654a780
dahyun Ham <dahuyn1@gmail.com>
2021-07-21 16:17:31 +0900

    use kakao api

---
+++ public/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { HashRouter, Route } from 'react-router-dom';
+import Home from './routes/Home';
+import About from './routes/About';
+import Navigation from './components/Navigation';
+
+const App = (props) => {
+    return (
+      <HashRouter>
+          <Navigation />
+            <Route path="/" exact={true} component={Home} />
+            <Route path="/about" component={About} />
+      </HashRouter>
+    )
+};
+
+export default App;
